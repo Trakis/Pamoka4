63a00765b276d2edb64e71b034863889d89cf465
Studentas1 <MyEmail@gmail.com>
2020-02-09 17:33:19 +0200

    Update

--- pythonFile.py
+++ pythonFile.py
@@ -1,1 +1,9 @@
 produktai=['Agurotis','Burokas','Duona','Citrina']
+print('Produktai pries rusiavima: ',produktai)
+
+temp=produktai[2]
+produktai[2]=produktai[3]
+
+produktai[3]=temp
+
+print('Produktai pries rusiavima: ',produktai)
